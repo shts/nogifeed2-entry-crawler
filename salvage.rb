4b8f14b7bb17592af9171333e6f7a226f144d6fd
shota-saito <s.saito@timers-inc.com>
2017-01-27 17:03:05 +0900

    tmp

---
+++ salvage.rb
@@ -0,0 +1,80 @@
+require 'bundler'
+require 'kconv'
+Bundler.require # gemを一括require
+
+require_relative 'useragent'
+require_relative 'htmlparser'
+require_relative 'downloader'
+require_relative 'crawler'
+require_relative 'xmlparser'
+require_relative 'tables'
+require_relative 'pusher'
+require_relative 'uploader'
+require 'date'
+
+def log text
+=begin
+  File.open("log.txt", "a") do |f|
+    f.puts(text)
+  end
+=end
+end
+
+def fetch(e)
+  url = e['url']
+  begin
+    d = Nokogiri::HTML(open(url, 'User-Agent' => UserAgents.agent))
+    #d.xpath('//*[@id="sheet"]/div[3]')
+    d.css('div.entrybottom').text
+  rescue OpenURI::HTTPError, URI::InvalidURIError, Net::OpenTimeout => ex
+    if ex == OpenURI::HTTPError then
+      puts "******************************************************************************************"
+      puts "HTTPError : url(#{url}) then #{ex.message} with retry!!!"
+      puts "******************************************************************************************"
+      #log "HTTPError : url(#{url}) then #{ex.message} with retry!!!"
+      sleep 5
+      retry
+    elsif ex == Net::OpenTimeout then
+      puts "******************************************************************************************"
+      puts "HTTPError : url(#{url}) then #{ex.message} with retry!!!"
+      puts "******************************************************************************************"
+      log "HTTPError : url(#{url}) then #{ex.message} with retry!!!"
+      sleep 5
+      retry
+    else
+      # 無効なURLは処理をせずnilを返却する
+      puts "******************************************************************************************"
+      puts "HTTPError : url(#{url}) then #{ex.message} !!!"
+      puts "******************************************************************************************"
+      if ex.message == "404 Not Found" then
+        e.destroy
+        log "HTTPError : url(#{url}) then #{ex.message}"
+      elsif ex.message == "403 Forbidden" then
+        sleep 5
+        retry
+      end
+    end
+  end
+end
+
+
+Api::Entry.all.each do |e|
+  # 403対策
+  sleep 5
+
+  s = fetch e['url']
+  if s == nil then
+    puts "published is nil"
+  else
+    d = DateTime.parse s
+    puts d
+    if d == nil then
+      puts "d is nil -> #{e['url']}"
+    else
+      e['published2'] = d
+      e.save
+    end
+  end
+end
+
+#puts fetch "http://blog.nogizaka46.com/yumi.wakatsuki/2017/01/036673.php"
